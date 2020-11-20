55da993ded8031c7b83b6ec34d6674920aaa25b3
Patrick Zhen <Zhen.Patrick@gmail.com>
2020-11-20 16:48:50 -0500

    Added Email Notifications.

--- email.py
+++ email.py
@@ -2,5 +2,19 @@ import smtplib
 from email.message import EmailMessage
 
 
-def Email_alert(subject, body, to):
-    message =
+def Email_alert(to, subject, body):
+    message = EmailMessage()
+    message.set_content(body)
+    message["subject"] = subject
+
+    email = ""
+    password = ""
+
+    server = smtplib.SMTP("smtp.gmail.com")
+    server.starttls()
+    server.login(email, password)
+
+    server.quit
+
+if __name__ == "__main__":
+    Email_alert("Zhen.Patrick@gmail.com", "Notification", "Your dumbbels have reached the desired price!")
