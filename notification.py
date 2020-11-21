749574a9e9e770d17e44a98c057a9a0ad99c05be
Patrick Zhen <Zhen.Patrick@gmail.com>
2020-11-21 18:50:44 -0500

    Fixed email password.

--- notification.py
+++ notification.py
@@ -5,16 +5,19 @@ from email.message import EmailMessage
 def Email_alert(to, subject, body):
     message = EmailMessage()
     message.set_content(body)
+    message["to"] = to
     message["subject"] = subject
-
-    email = ""
+    
+    email = "Aniangelsuperbat@gmail.com"
+    message["from"] = email
     password = ""
 
     server = smtplib.SMTP("smtp.gmail.com")
     server.starttls()
     server.login(email, password)
-
-    server.quit
+    server.send_message(message)
+    
+    server.quit()
 
 if __name__ == "__main__":
     Email_alert("Zhen.Patrick@gmail.com", "Notification", "Your dumbbels have reached the desired price!")
